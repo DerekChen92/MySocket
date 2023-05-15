99ae498b44098c064dff4987c741776c0ce034f4
Derek Chen <derek.chen@mindtronicai.com>
2023-05-15 11:21:12 +0800

    initialize repo

---
+++ inc/my_socket_client.hpp
@@ -0,0 +1,6 @@
+#ifndef __MY_SOCKET_CLIENT_HPP
+#define __MY_SOCKET_CLIENT_HPP
+
+class MySocketClient {};
+
+#endif // __MY_SOCKET_CLIENT_HPP
---
+++ inc/my_socket_server.hpp
@@ -0,0 +1,6 @@
+#ifndef __MY_SOCKET_SERVER_HPP
+#define __MY_SOCKET_SERVER_HPP
+
+class MySocketServer {};
+
+#endif // __MY_SOCKET_SERVER_HPP
