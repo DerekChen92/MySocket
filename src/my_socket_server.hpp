5c69f8d4aa68903b3370493b822f2ac3f22f7fbe
DerekChen <chingyuchen92@gmail.com>
2023-06-10 17:55:55 +0800

    stash

--- src/my_socket_server.hpp
+++ src/my_socket_server.hpp
@@ -1,6 +1,31 @@
 #ifndef __MY_SOCKET_SERVER_HPP
 #define __MY_SOCKET_SERVER_HPP
 
-class MySocketServer {};
+#include <sys/socket.h>
+
+#include "my_socket_error.hpp"
+
+
+namespace MySocket
+{
+class Server {
+    public:
+    	Server() {
+    		
+    	}
+
+        int create() {
+            if (_fd > 0) {
+                return Error::ServerCreated;
+            }
+        }
+    private:  
+        int _fd;
+};
+}
+
+
+
+
 
 #endif // __MY_SOCKET_SERVER_HPP
